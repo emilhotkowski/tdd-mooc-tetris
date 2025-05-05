77121a3c025a471c17bd66becb25372e7a052e1a
Emil Hotkowski <ehotkowski@hopper.com>
2025-05-05 21:54:04 +0200

    feat: Move blocks

--- src/Board.mjs
+++ src/Board.mjs
@@ -16,6 +16,15 @@ export class Board {
     this.board[0][1] = block;
   }
 
+  tick() {
+    for(var i = this.height-2; i >= 0; i--)  // TODO: make sure height is at least 1
+      for(var j = 0; j < this.width; j++)
+        if(this.board[i][j] == 'X' && this.board[i+1][j] =='.') {
+          this.board[i][j] = '.';
+          this.board[i+1][j] = 'X';
+        }
+  }
+
   toString() {
     return this.board.map((x) => x.join("")).join("\n") + "\n";
   }
