dbe7013243593f3b03afa4a815026584efe9d90a
Emil Hotkowski <ehotkowski@hopper.com>
2025-05-05 22:22:48 +0200

    feat: stop on another blocks

--- test/FallingBlocks.test.mjs
+++ test/FallingBlocks.test.mjs
@@ -76,7 +76,7 @@ describe("Falling blocks", () => {
     });
   });
 
-  /*
+
   describe("When a block lands on another block", () => {
     beforeEach(() => {
       board.drop("X");
@@ -96,7 +96,7 @@ describe("Falling blocks", () => {
       expect(board.hasFalling(), "the player should still be able to move the block").to.be.true;
     });
 
-    test.skip("it stops when it hits the other block", () => {
+    test("it stops when it hits the other block", () => {
       board.tick();
 
       expect(board.toString()).to.equalShape(
@@ -107,5 +107,4 @@ describe("Falling blocks", () => {
       expect(board.hasFalling(), "the block should stop moving").to.be.false;
     });
   });
-  */
 });
