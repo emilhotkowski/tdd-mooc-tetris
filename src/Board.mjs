6b7a461f7922c97bba6b16c18c355caeb3f278d0
Emil Hotkowski <ehotkowski@hopper.com>
2025-05-05 21:40:31 +0200

    feat: Adding block

--- src/Board.mjs
+++ src/Board.mjs
@@ -5,9 +5,18 @@ export class Board {
   constructor(width, height) {
     this.width = width;
     this.height = height;
+    this.board = new Array(height);
+    for(var i = 0; i < height; i++) {
+      this.board[i] = new Array(width);
+      this.board[i].fill(".");
+    }
+  }
+
+  drop(block) {
+    this.board[0][1] = block;
   }
 
   toString() {
-    return "TODO";
+    return this.board.map((x) => x.join("")).join("\n") + "\n";
   }
 }
